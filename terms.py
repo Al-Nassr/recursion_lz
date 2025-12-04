9409ddecabbaa00d6e7720ac953276a1b0fc2c8e
Al-Nassr <moisescaicedo@tutamail.com>
2025-12-04 15:43:17 +0300

    Update terms.py

--- terms.py
+++ terms.py
@@ -1,15 +1,18 @@
-def partition(n, min_number = 1):
+python
+def partition(n, min_number=1):
     if n == 0:
-        return [[]]  # Базовый случай: если осталась ноль, возвращаем пустое разбиение
+        return [[]]  # Базовый случай: если осталось 0, возвращаем пустое разбиение
     parts = []
     for i in range(min_number, n + 1):  # Проходим от min_number до n
-        for part in partition(n - i, i):  # Рекурсивный вызов для оставшегося числа
+        for part in partition(n - i, i):  # Рекурсивный вызов для остатка
             parts.append([i] + part)  # Добавляем текущее число к каждому разбиению
     return parts
-
 def print_partitions(n):
     partitions = partition(n)
     for p in partitions:
         print(p)
-
-print_partitions(int(input('Введите число, которое хотите разложить на сумму целых чисел ')))
+def main():
+    n = int(input("Введите число, которое хотите разложить на сумму целых чисел: "))
+    print_partitions(n)
+if __name__ == "__main__":
+    main()
