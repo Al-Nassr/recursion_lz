f94bc8d4914165f5292315a142abf5086f071671
Al-Nassr <moisescaicedo@tutamail.com>
2025-12-04 15:44:18 +0300

    Update pascal.py

--- pascal.py
+++ pascal.py
@@ -5,17 +5,15 @@ def pascal_triangle(n):
         result = pascal_triangle(n - 1)
         current_row = [1]
         previous_row = result[-1]
-
         for i in range(len(previous_row) - 1):
             current_row.append(previous_row[i] + previous_row[i + 1])
-
         current_row.append(1)
         result.append(current_row)
         return result
-
 def main():
     triangle = pascal_triangle(5)
     for row in triangle:
         print(row)
 if __name__ == "__main__":
     main()
+
