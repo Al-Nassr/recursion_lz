f4b953a2b26ed6cbfdf1647215302cc45df00753
Al-Nassr <moisescaicedo@tutamail.com>
2025-12-04 15:31:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import Euclid
+import pascal
+import perfection
+import terms
+
+
+def main():
+    Euclid.euc()
+    pascal.pasc()
+    perfection.perf()
+    terms.trm()
+
+if __name__ == '__main__':
+    main()
