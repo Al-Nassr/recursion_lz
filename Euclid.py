6e6d8572ff45682661a33dd06d17c108487f625e
Al-Nassr <moisescaicedo@tutamail.com>
2025-12-04 15:41:44 +0300

    Update Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -2,12 +2,15 @@ def gcd(a, b):
     # Базовый случай: если b равно 0, НОД равен a
     if b == 0:
         return a
-    # Рекурсивный случай: продолжаем алгоритм Евклида
-    else:
-        return gcd(b, int(a) % int(b))
-print("введите первое число")
-user_input_1 = input()
-print("введите второе число")
-user_input_2 = input()
-result = gcd(user_input_1,user_input_2)
-print("НОД чисел равен",result)
+    # Рекурсивный случай: алгоритм Евклида
+    return gcd(b, a % b)
+def main():
+    print("Введите первое число:")
+    user_input_1 = int(input())
+
+    print("Введите второе число:")
+    user_input_2 = int(input())
+    result = gcd(user_input_1, user_input_2)
+    print("НОД чисел равен", result)
+if __name__ == "__main__":
+    main()
